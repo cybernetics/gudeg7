9fe44edd2f8ac417e3a9fec9aa8864ed28f34be2
Yosi <yosi.pramajaya@gmail.com>
2013-07-16 10:18:29 +0700

    Trying JDK8 EA in Gudeg7

--- gudeg7-ejb/src/main/java/com/jug/joglosemar/model/Member.java
+++ gudeg7-ejb/src/main/java/com/jug/joglosemar/model/Member.java
@@ -19,7 +19,10 @@ import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlTransient;
 
 /**
  *
@@ -82,6 +85,7 @@ public class Member implements Serializable {
     private String province;
     
     @Transient
+    @XmlTransient
     private boolean selected;
 
     public Long getId() {
