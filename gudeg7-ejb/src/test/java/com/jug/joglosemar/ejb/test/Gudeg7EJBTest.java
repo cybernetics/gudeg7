816fcad38c4b5aea3715a221fd28302252739b26
Yosi <yosi.pramajaya@gmail.com>
2013-07-19 08:06:29 +0700

    Integrating with Arquillian Tester!

---
+++ gudeg7-ejb/src/test/java/com/jug/joglosemar/ejb/test/Gudeg7EJBTest.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.jug.joglosemar.ejb.test;
+
+import org.junit.Test;
+
+/**
+ *
+ * @author Yosi Pramajaya
+ */
+public class Gudeg7EJBTest {
+    
+    @Test
+    public void ejbTest() {
+        System.out.println("Hello Arquillian. Integration with Gudeg7-EJB");
+    }
+}
---
+++ gudeg7-web/src/test/java/com/jug/joglosemar/test/Gudeg7Test.java
@@ -0,0 +1,26 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.jug.joglosemar.test;
+
+import org.jboss.arquillian.junit.Arquillian;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ *
+ * @author Yosi Pramajaya
+ */
+@RunWith(Arquillian.class)
+public class Gudeg7Test {
+    
+    @Test
+    public void gudeg7Test() {
+        System.out.println("Hello Arquillian. Integration with Gudeg7");
+    }
+}
